8dded33f37a3a1d18c057dfbf274e9829e99da6f
Federico Fuga <fuga@studiofuga.com>
2017-11-14 18:06:21 +0100

    Few table testing added

--- tests/table.cpp
+++ tests/table.cpp
@@ -28,3 +28,42 @@ TEST(table, creation)
 
     ASSERT_TRUE(db->tableExists("sample"));
 }
+
+class MyTable : public SQLiteTable {
+private:
+    FieldDef<FieldType::Integer> mId = makeFieldDef("id", FieldType::Integer()).primaryKey().autoincrement();
+    FieldDef<FieldType::Text> mName = makeFieldDef("name", FieldType::Text());
+    FieldDef<FieldType::Integer> mValue = makeFieldDef("value", FieldType::Integer());
+public:
+    MyTable(std::shared_ptr<SQLiteStorage> db)
+            : SQLiteTable(db, "records")
+    {
+    }
+
+    void create() {
+        SQLiteTable::create(std::make_tuple(mId, mName, mValue));
+    }
+
+    struct Record {
+        std::string name;
+        int value;
+    };
+
+    void insert(const Record &record) {
+        SQLiteTable::insert(std::make_tuple(mName, mValue), std::make_tuple(record.name, record.value));
+    }
+
+};
+
+TEST(table, subclassing)
+{
+    auto db = std::make_shared<SQLiteStorage>(":memory:");
+
+    ASSERT_NO_THROW(db->open());
+
+    MyTable myTable(db);
+
+    ASSERT_NO_THROW(myTable.create());
+    ASSERT_NO_THROW(myTable.insert(MyTable::Record{"First", 2000}));
+    ASSERT_NO_THROW(myTable.insert(MyTable::Record{"Second", 0}));
+}
