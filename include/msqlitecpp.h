4e166bad5aaa97acf36973f5337aacc8e84a0dfa
Federico Fuga <fuga@studiofuga.com>
2018-02-08 19:23:35 +0100

    Added CMake option to embed msqlite-amalgamation

--- include/msqlitecpp.h
+++ include/msqlitecpp.h
@@ -2,13 +2,17 @@
 #define MSQLITECPP_H
 
 #ifdef _WIN32
-#ifdef BUILD_MSQLITECPP
-#define EXPORT __declspec(dllexport)
+    #ifdef BUILD_MSQLITECPP
+        #define EXPORT __declspec(dllexport)
+    #else
+        #ifdef EMBED_MSQLITECPP
+            #define EXPORT
+        #else
+            #define EXPORT __declspec(dllimport)
+        #endif
+    #endif
 #else
-#define EXPORT __declspec(dllimport)
-#endif
-#else
-#define EXPORT
+    #define EXPORT
 #endif
 
 #endif // MSQLITECPP_H
