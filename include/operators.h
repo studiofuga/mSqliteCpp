927ead7231202205aab4fb87b1a5f488e4af2dab
ing. Federico Fuga <fuga@studiofuga.com>
2018-07-10 12:06:25 +0200

    Added new class And for operators

--- include/operators.h
+++ include/operators.h
@@ -27,7 +27,7 @@ class Eq {
 public:
     Eq(FieldDef <FT> f) : fld(std::move(f)) {}
 
-    std::string operator() (int indx = 0){
+    std::string operator() (int indx = 0) const {
         return details::make_oper(fld, "=", indx);
     }
 };
@@ -38,8 +38,33 @@ class Ne {
 public:
     Ne(FieldDef <FT> f) : fld(std::move(f)) {}
 
-    std::string operator() (int indx = 0){
+    std::string operator() (int indx = 0) const {
         return details::make_oper(fld, "<>", indx);
+    }
+};
+
+template <typename ...OPS>
+class And {
+    const std::tuple<OPS...> fields;
+
+    template <size_t I, typename std::enable_if<I == sizeof...(OPS), int>::type = 0 >
+    std::string opImpl(int idx) {
+        return std::string();
+    }
+
+    template <size_t I, typename std::enable_if<I < sizeof...(OPS), int>::type = 0 >
+    std::string opImpl(int idx) {
+        auto &f = std::get<I>(fields);
+        return (I == 0 ? "" : " AND ") + f(I+idx) + opImpl<I+1>(idx);
+    }
+
+
+public:
+    And (OPS... f) : fields(std::make_tuple(f...)) {
+    }
+
+    std::string operator() (int idx = 0){
+        return opImpl<0>(idx);
     }
 };
 
@@ -52,6 +77,11 @@ Ne<FT> ne(const FieldDef <FT> &field)
 template <typename FT>
 Eq<FT> eq(FieldDef<FT> f) {
     return Eq<FT>(f);
+}
+
+template<typename ...OPS>
+And<OPS...> and_(OPS... ops) {
+    return And<OPS...>(ops...);
 }
 
 } // op
--- tests/updatestatements.cpp
+++ tests/updatestatements.cpp
@@ -17,7 +17,7 @@ class UpdateStatements : public testing::Test {
 protected:
     std::shared_ptr<SQLiteStorage> db;
 public:
-    FieldDef<FieldType::Integer> fldId{"id", PrimaryKey};
+    FieldDef<FieldType::Integer> fldId{"id"};
     FieldDef<FieldType::Integer> fldId2{"id2"};
     FieldDef<FieldType::Text> fldName{"n"};
     FieldDef<FieldType::Integer> fldValue{"v"};
@@ -51,6 +51,8 @@ protected:
         stmt.bind(std::make_tuple(2, 2, "name2", 4));
         stmt.execute();
         stmt.bind(std::make_tuple(3, 2, "name3", 6));
+        stmt.execute();
+        stmt.bind(std::make_tuple(3, 3, "name4", 8));
         stmt.execute();
 
         Test::SetUp();
@@ -165,6 +167,64 @@ TEST_F(UpdateStatements, updateWithOptional)
     });
 
     ASSERT_TRUE(ok);
+}
+
+TEST_F(UpdateStatements, updateWithOptionalMore)
+{
+    UpdateStatement<
+            decltype(fldName), decltype(fldValue)
+    > updateStatement(fldName, fldValue);
+
+    updateStatement.attach(db, tablename);
+
+    auto where = makeWhere(updateStatement.getStatement(), operators::and_(operators::eq(fldId),operators::eq(fldId2)));
+
+    updateStatement.where(where);
+
+    int id = 3, id2 = 3;
+    boost::optional<int> value = 1000;
+    boost::optional<std::string> name {"aaa"};
+
+    ASSERT_NO_THROW(updateStatement.prepare(name, value));
+    ASSERT_NO_THROW(where.bind(id, id2));
+    ASSERT_NO_THROW(updateStatement.update(name, value));
+
+    SelectStatement<
+            decltype(fldId), decltype(fldId2), decltype(fldName), decltype(fldValue)
+    > selectStatement(fldId, fldId2, fldName, fldValue);
+    selectStatement.attach(db, tablename);
+    Where<decltype(fldId)> selectWhere;
+
+    selectWhere.attach(selectStatement.getStatement(), operators::eq(fldId)());
+    selectStatement.where(selectWhere);
+    selectStatement.prepare();
+
+    selectWhere.bind(id);
+    int rid_2, rid2_2, rvalue_2;
+    int rid_3, rid2_3, rvalue_3;
+    bool ok = true;
+    selectStatement.exec([&rid_2, &rid_3, &rid2_2, &rid2_3, &rvalue_2, &rvalue_3, &ok](int id, int id2, std::string name, int value) {
+        if (id2 == 2) {
+            rid_2 = id;
+            rid2_2 = id2;
+            rvalue_2 = value;
+        } else if (id2 == 3){
+            rid_3 = id;
+            rid2_3 = id2;
+            rvalue_3 = value;
+        } else {
+            ok = false;
+        }
+        return true;
+    });
+
+    ASSERT_TRUE(ok);
+    ASSERT_EQ(rid_2, id);
+    ASSERT_EQ(rid_3, id);
+    ASSERT_EQ(rid2_2, 2);
+    ASSERT_EQ(rid2_3, id2);
+    ASSERT_EQ(rvalue_2, 6);
+    ASSERT_EQ(rvalue_3, 1000);
 }
 
 
