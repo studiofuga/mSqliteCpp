8c94fbd59384afe7e08683e7d2d5801ef55b9152
Federico Fuga <fuga@studiofuga.com>
2019-01-11 23:14:41 +0100

    vcpkg scripts updated for latest tag v0.9.3

--- vcpkg/msqlitecpp/portfile.cmake
+++ vcpkg/msqlitecpp/portfile.cmake
@@ -11,18 +11,24 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/mSqliteCpp-0.9.1)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/mSqliteCpp-0.9.3)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/studiofuga/mSqliteCpp/archive/v0.9.1.tar.gz"
-    FILENAME "mSqliteCpp-0.9.1.tar.gz"
-    SHA512 9ce29ff834dd3279e796adffdaeb4fdc1522e86b8eaa55dd1548254f8fd9cc93d0b1c9840e1982318fa7e4afd373de76d2933e150215fe887c2307da96145d97
+    URLS "https://github.com/studiofuga/mSqliteCpp/archive/v0.9.3.tar.gz"
+    FILENAME "mSqliteCpp-0.9.3.tar.gz"
+    SHA512 c9309caf301a1973816ab51411dd141085b4e9ac4221de7403d6fd96cac9e9c5a849da8fa72989f71af5fc52ad31c538e35a4d28c64a7d5072dc658dbffc252e
 )
 vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_apply_patches(
+	SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+        ${CMAKE_CURRENT_LIST_DIR}/0000-FixC1001.patch
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DENABLE_TEST=OFF -DENABLE_PROFILER=OFF -DENABLE_CODE_COVERAGE=OFF
+    OPTIONS -DENABLE_TEST=OFF -DENABLE_PROFILER=OFF -DENABLE_SQLITE_AMALGAMATION=OFF -DENABLE_CODE_COVERAGE=OFF
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
 )
