20e1113252b4cb2875e6a8f9626d272af7661b9e
Federico Fuga <fuga@studiofuga.com>
2025-12-22 12:06:53 +0100

    Enable sanitization in debug compilation

--- cmake/compileropts.cmake
+++ cmake/compileropts.cmake
@@ -9,3 +9,22 @@ if (APPLE)
 endif (APPLE)
 
 set(CMAKE_POSITION_INDEPENDENT_CODE on)
+
+if ("${CMAKE_BUILD_TYPE}" MATCHES "Debug")
+    set(ASAN_ENABLED ON)
+    message("Address Sanitizer: ENABLED")
+endif ()
+
+if (ASAN_ENABLED)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined,leak -fno-omit-frame-pointer")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=address,undefined,leak -fno-omit-frame-pointer")
+
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address,undefined,leak")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=address,undefined,leak")
+
+    add_compile_definitions(-DASAN_ENABLED)
+endif()
+
+# Always ensure debug symbols and frame pointers in Debug builds
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -ggdb")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -ggdb")
